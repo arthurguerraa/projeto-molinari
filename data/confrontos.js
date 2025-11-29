463d70a7d711d58eb26fe16fea6192a2d4508254
arthurguerraa <127884471+arthurguerraa@users.noreply.github.com>
2025-11-29 16:01:40 -0300

    fake data tabela de confrontos da rodada

---
+++ data/confrontos.js
@@ -0,0 +1,32 @@
+const confrontos = [
+    { casa: "Arsenal", fora: "Chelsea", data: "30/11" },
+    { casa: "Liverpool", fora: "Manchester City", data: "30/11" },
+    { casa: "Tottenham", fora: "Newcastle", data: "01/12" },
+    { casa: "Manchester United", fora: "Everton", data: "01/12" },
+    { casa: "Aston Villa", fora: "Brighton", data: "01/12" },
+    { casa: "West Ham", fora: "Brentford", data: "02/12" },
+    { casa: "Fulham", fora: "Crystal Palace", data: "02/12" },
+    { casa: "Wolves", fora: "Bournemouth", data: "02/12" },
+    { casa: "Leicester", fora: "Southampton", data: "03/12" },
+    { casa: "Nottingham Forest", fora: "Ipswich Town", data: "03/12" },
+];
+
+function carregarConfrontos() {
+    const tbody = document.getElementById("matches-body");
+
+    confrontos.forEach(jogo => {
+        const row = document.createElement("tr");
+
+        row.innerHTML = `
+            <td>${jogo.casa}</td>
+            <td>x</td>
+            <td>${jogo.fora}</td>
+            <td>${jogo.data}</td>
+        `;
+
+        tbody.appendChild(row);
+    });
+}
+
+carregarConfrontos();
+
